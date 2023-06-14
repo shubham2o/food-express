a68e6a15635bf33a176e81b92592516432a8816b
shubham2o <shubhamsharma.ha@gmail.com>
2023-06-14 19:09:15 +0530

    Refractor Code.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
-    return (
-      <div className="footer">
-        <div>This is Footer.</div>
-      </div>
-    );
-  };
+  return (
+    <div className="footer">
+      <div>This is Footer.</div>
+    </div>
+  );
+};
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,27 @@
 import { LOGO_URL } from "../utils/constants";
 
 const Title = () => {
-    return (
-      <a href="/" className="logo-container">
-        <img
-          className="logo"
-          src={LOGO_URL}
-          alt="Food Express Logo"
-        />
-      </a>
-    );
-  }
+  return (
+    <a href="/" className="logo-container">
+      <img className="logo" src={LOGO_URL} alt="Food Express Logo"/>
+    </a>
+  );
+}
 
 const Header = () => {
-    return (
-      <div className="header">
-        <Title/>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  return (
+    <div className="header">
+      <Title/>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Header;
