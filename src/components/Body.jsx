3d00bb830c99861e64b79226488be669c47166b2
shubham2o <shubhamsharma.ha@gmail.com>
2023-06-14 19:02:40 +0530

    Button created - Top Rated Restaurants (useState(), filter() method implemented in it)

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,29 @@
-import restaurantsList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import restaurantsList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">Search</div>
-        <div className="res-container">
-          {
-            restaurantsList.map((restaurant) => {
-                return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />;
-            })
-          }
-        </div>
+  const [listOfRestaurants, setListOfRestaurants] = useState(restaurantsList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button className="filter-btn" 
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter((res) => res.data.avgRating > 4);
+            setListOfRestaurants(filteredList);
+          }} 
+        > Top Rated Restaurants 
+        </button>
       </div>
-    );
-  };
+      <div className="res-container">
+        { listOfRestaurants.map((restaurant) => {
+              return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />;
+          })
+        }
+      </div>
+    </div>
+  );
+};
 
 export default Body;
