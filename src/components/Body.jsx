f7ed367dc43d00ac712a520b3cc5668c3e250bde
shubham2o <shubhamsharma.ha@gmail.com>
2023-06-19 11:54:05 +0530

    Added useEffect(), fetchData() for live API data call and because of this removed mockData.js from src\utils

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,20 @@
 import RestaurantCard from "./RestaurantCard";
-import restaurantsList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(restaurantsList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(()=> {fetchData()}, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6406443&lng=77.2746373&page_type=DESKTOP_WEB_LISTING"
+      );
+    const json = await data.json();
+    console.log(json);
+    // Optional Chaining
+    setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
 
   return (
     <div className="body">
