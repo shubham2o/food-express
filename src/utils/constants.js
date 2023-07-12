31b743018f78542261e0f15bbe1b427368e009cc
shubham2o <shubhamsharma.ha@gmail.com>
2023-07-12 20:27:41 +0530

    MENU_API link separately added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
-export const LOGO_URL = 
+export const LOGO_URL =
   "https://dcassetcdn.com/design_img/1889677/444768/444768_10393234_1889677_85203911_image.png";
 
-export const CDN_URL = 
+export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6406443&lng=77.2746373&restaurantId=";
